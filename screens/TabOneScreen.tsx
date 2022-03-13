adb1c1d5f9ea52a8ed57dfbd4ddb22bbcc77a449
Kaung Myat Han <arkarhtethan@gmail.com>
2022-03-13 06:53:59 +0630

    show room list

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,16 +1,31 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, FlatList } from 'react-native';
 import ChatRoomItem from '../components/ChatRoomItem/ChatRoomItem';
-
 import { RootTabScreenProps } from '../types';
+import ChatRoomsData from "../assets/dummy-data/ChatRooms";
+
+const chat = ChatRoomsData[0];
+const chat1 = ChatRoomsData[2];
 
 export default function TabOneScreen ({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
     <View style={styles.page}>
-      <ChatRoomItem />
-      <ChatRoomItem />
-      <ChatRoomItem />
-      <ChatRoomItem />
-      <ChatRoomItem />
+      <FlatList
+        data={ChatRoomsData}
+        renderItem={({ item }) => <ChatRoomItem chatRoom={item} />}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+      />
+      <FlatList
+        data={ChatRoomsData}
+        showsVerticalScrollIndicator={false}
+        renderItem={({ item }) => <ChatRoomItem chatRoom={item} />}
+        ListHeaderComponent={() => (<FlatList
+          data={ChatRoomsData}
+          renderItem={({ item }) => <ChatRoomItem chatRoom={item} />}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+        />)}
+      />
     </View>
   );
 }
