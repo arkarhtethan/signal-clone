84206d73d1cd9227e7ba9bce4067ba8d1d011f25
Kaung Myat Han <arkarhtethan@gmail.com>
2022-03-15 10:48:00 +0630

    HomeScreen added

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,29 @@
+import { StyleSheet, View, FlatList } from 'react-native';
+import ChatRoomItem from '../components/ChatRoomItem/ChatRoomItem';
+import { RootTabScreenProps } from '../types';
+import ChatRoomsData from "../assets/dummy-data/ChatRooms";
+
+const chat = ChatRoomsData[0];
+const chat1 = ChatRoomsData[2];
+
+export default function HomeScreen ({ navigation }: RootTabScreenProps<'TabOne'>) {
+  return (
+    <View style={styles.page}>
+      <FlatList
+        data={ChatRoomsData}
+        showsVerticalScrollIndicator={false}
+        renderItem={({ item }) => <ChatRoomItem chatRoom={item} />}
+        ListHeaderComponent={() => (<FlatList
+          data={ChatRoomsData}
+          renderItem={({ item }) => <ChatRoomItem chatRoom={item} />}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+        />)}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  page: {}
+});
