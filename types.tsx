9361248185a2c090a956348cc296690ae2980f02
Kaung Myat Han <arkarhtethan@gmail.com>
2022-03-15 10:45:53 +0630

    modal , root removed

--- types.tsx
+++ types.tsx
@@ -9,14 +9,14 @@ import { NativeStackScreenProps } from '@react-navigation/native-stack';
 
 declare global {
   namespace ReactNavigation {
-    interface RootParamList extends RootStackParamList {}
+    interface RootParamList extends RootStackParamList { }
   }
 }
 
 export type RootStackParamList = {
-  Root: NavigatorScreenParams<RootTabParamList> | undefined;
-  Modal: undefined;
+  Home: undefined;
   NotFound: undefined;
+  ChatRoom: { id: any };
 };
 
 export type RootStackScreenProps<Screen extends keyof RootStackParamList> = NativeStackScreenProps<
