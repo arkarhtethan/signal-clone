b0195507760283f7e044cf7d19cc53ff0a3b8c05
Kaung Myat Han <arkarhtethan@gmail.com>
2022-03-21 00:47:51 +0630

    replace with users from datastore

--- screens/UsersScreen.tsx
+++ screens/UsersScreen.tsx
@@ -1,15 +1,24 @@
 import { StyleSheet, View, FlatList } from 'react-native';
 import UserItem from '../components/UserItem';
 import { RootTabScreenProps } from '../types';
-import Users from "../assets/dummy-data/Users";
+import { useEffect, useState } from 'react';
+import { DataStore } from '@aws-amplify/datastore';
+import { User } from '../src/models';
 
 export default function UsersScreen ({ navigation }: RootTabScreenProps<'TabOne'>) {
+
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    DataStore.query(User).then(setUsers);
+  }, [])
+
   return (
     <View style={styles.page}>
       <FlatList
-        data={Users}
+        data={users}
         showsVerticalScrollIndicator={false}
-        renderItem={({ item }) => <UserItem user={item} />}
+        renderItem={({ item }) => <UserItem user={{ ...item }} />}
       />
     </View>
   );
