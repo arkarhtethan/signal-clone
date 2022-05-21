9b9bfc17107b4c372297ff57826c2a08720a3c5e
Kaung Myat Han <arkarhtethan@gmail.com>
2022-05-22 01:22:50 +0630

    user status / last seen online

--- App.tsx
+++ App.tsx
@@ -1,21 +1,58 @@
-import { Amplify, DataStore, Hub } from "aws-amplify";
+import { Amplify, Auth, DataStore, Hub } from "aws-amplify";
 // @ts-ignore
 import { withAuthenticator } from 'aws-amplify-react-native';
 import { StatusBar } from 'expo-status-bar';
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import 'react-native-get-random-values';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import awsconfig from './aws-exports';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
-import { Message } from "./src/models";
+import { Message, User } from "./src/models";
 
 Amplify.configure(awsconfig)
 
 function App () {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    fetchUser();
+  }, [])
+
+  const fetchUser = async () => {
+    const userData = await Auth.currentAuthenticatedUser();
+    const user = await DataStore.query(User, userData.attributes.sub);
+    if (user) {
+      setUser(user)
+    }
+  }
+
+  useEffect(() => {
+    if (!user) return;
+    const subscription = DataStore.observe(User, user.id).subscribe((msg) => {
+      if (msg.model === User && msg.opType === "UPDATE") {
+        setUser((curUser: any) => ({ ...curUser, ...msg.element, }))
+      }
+    })
+    return () => subscription.unsubscribe();
+  }, [])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      updateLastOnline();
+    }, 1 * 60 * 1000)
+    return () => clearInterval(interval)
+  }, [user])
+
+
+  const updateLastOnline = async () => {
+    if (!user) return;
+    await DataStore.save(User.copyOf(user, updated => { updated.lastOnlineAt = +(new Date()) }))
+  }
+
 
   useEffect(() => {
     const listener = Hub.listen('datastore', async hubData => {
--- navigation/ChatRoomHeader.tsx
+++ navigation/ChatRoomHeader.tsx
@@ -1,5 +1,6 @@
 import { Feather } from "@expo/vector-icons";
 import { Auth, DataStore } from "aws-amplify";
+import moment from "moment";
 import * as React from 'react';
 import { Image, Text, useWindowDimensions, View } from 'react-native';
 import { ChatRoomUser, User } from "../src/models";
@@ -21,12 +22,26 @@ export const ChatRoomHeader = (props: any) => {
 
     }, [])
 
+    const getLastOnlineText = () => {
+        if (!user?.lastOnlineAt) {
+            return null;
+        }
+        const lastOnlineDiffMs = moment().diff(moment(user.lastOnlineAt));
+        if (lastOnlineDiffMs < 5 * 60 * 1000) {
+            return "online"
+        } else {
+            return `Last seen online ${moment(user.lastOnlineAt).fromNow()}`;
+        }
+    }
 
     return (<View style={{ flexDirection: 'row', padding: 10, alignItems: 'center', justifyContent: 'space-between', width: width - 75, marginRight: 10 }}>
         <Image
             source={{ uri: user?.imageUri }}
             style={{ width: 30, height: 30, borderRadius: 30, }} />
-        <Text style={{ fontWeight: 'bold' }}>{user?.name}</Text>
+        <View style={{ flex: 1, marginLeft: 10 }}>
+            <Text style={{ fontWeight: 'bold' }}>{user?.name}</Text>
+            <Text>{getLastOnlineText()}</Text>
+        </View>
         <View style={{ flexDirection: "row" }}>
             <Feather
                 name="camera"
